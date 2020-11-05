feb6a07d8ea7cac823a29a8cafeb4d5d3db69556
Felipe Borges <lipex360@gmail.com>
2020-11-05 13:29:44 -0300

    Entity Customer

---
+++ src/modules/customers/entities/Customer.ts
@@ -0,0 +1,32 @@
+import { v4 as uuid } from 'uuid'
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert
+} from 'typeorm'
+
+@Entity('customers')
+export default class Customer {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @BeforeInsert()
+  customerProps () {
+    this.id = uuid()
+  }
+}
