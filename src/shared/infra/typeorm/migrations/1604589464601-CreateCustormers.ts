678e9d2b0c6b106b6d12408f43ad959f3d26d98f
Felipe Borges <lipex360@gmail.com>
2020-11-06 12:30:28 -0300

    Rename Migration

--- src/shared/infra/typeorm/migrations/1604589464601-CreateCustormers.ts
+++ src/shared/infra/typeorm/migrations/1604589464601-CreateCustormers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
-export default class AddCustomer20201105124648 implements MigrationInterface {
+export default class CreateCustormers20201105124648 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
